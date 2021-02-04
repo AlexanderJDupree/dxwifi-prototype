00bafbb4a52ff8df335cec200e64f443ca9c221a
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-04 10:01:14 -0800

    Fixed monitor script

--- startMonitor.sh
+++ startMonitor.sh
@@ -9,25 +9,10 @@ set -e
 
 dev=${1:-mon0}
 
-<<<<<<< HEAD
-ip link set $dev down
-if [ $? -eq 0 ]; then
-	iw dev $dev set monitor fcsfail otherbss
-	ip link set $dev up
-	iw dev $dev set channel ${2:-3}
-	echo -e "Enabled $dev in monitor mode"
-	exit 0
-else
-	echo -e "Error, could not find device $dev"
-	echo -e "Usage: sudo startMonitor.sh [device:mon0] [channel:3]"
-	exit 1
-fi
-=======
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
->>>>>>> Got a working injection pipline prototyped
 
 ip link set $dev down
 iw dev $dev set monitor fcsfail otherbss
