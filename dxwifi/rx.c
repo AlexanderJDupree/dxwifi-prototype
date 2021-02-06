03934609b63e891995eb61c94ba04433228489d8
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-06 14:06:10 -0800

    Started on Rx program

---
+++ dxwifi/rx.c
@@ -0,0 +1,20 @@
+/**
+ *  Entry point for DxWifi packet capture
+ */
+
+
+#include <libdxwifi/dxwifi.h>
+
+
+typedef struct {
+    int file;
+    dxwifi_receiver rx;
+} cli_args;
+
+
+int main(int arc, char** argv) {
+
+    int status = 0;
+
+    return status;
+}
