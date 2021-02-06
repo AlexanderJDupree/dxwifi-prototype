0d69ca690634e458d4537be6ab43a40d3e500b9a
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-06 13:47:51 -0800

    Updated status messages for monitor script

--- startMonitor.sh
+++ startMonitor.sh
@@ -9,14 +9,23 @@ set -e
 
 dev=${1:-mon0}
 
+function cleanup()
+{
+    if [[ $? -ne 0 ]]; then
+        echo -e "Usage: sudo ./startMonitor.sh [interface:mon0] [channel:3] [txpower:20]"
+    else
+        echo -e "Enabled $dev in monitor mode"
+    fi
+}
+
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap cleanup EXIT
 
 ip link set $dev down
 iw dev $dev set monitor fcsfail otherbss
 ip link set $dev up
 iw dev $dev set channel ${2:-3}
-echo -e "Enabled $dev in monitor mode"
+iw dev $dev info
 exit 0
